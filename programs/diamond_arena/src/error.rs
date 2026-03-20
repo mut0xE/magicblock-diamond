b2ae957fda419c0333d751f7886ef7354f985506
“mut0xE” <210969342+mut0xE@users.noreply.github.com>
2026-03-20 13:55:28 +0530

    feat: added custom error

--- programs/diamond_arena/src/error.rs
+++ programs/diamond_arena/src/error.rs
@@ -1,1 +1,19 @@
+use anchor_lang::prelude::*;
 
+#[error_code]
+pub enum DiamondError {
+    #[msg("Invalid max players.")]
+    InvalidMaxPlayers,
+
+    #[msg("Room is already full.")]
+    RoomFull,
+
+    #[msg("Player already joined.")]
+    PlayerAlreadyJoined,
+
+    #[msg("Room is not in waiting state.")]
+    RoomNotWaiting,
+
+    #[msg("Math overflow.")]
+    MathOverflow,
+}
